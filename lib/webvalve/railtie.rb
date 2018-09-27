bf6c118afce1d6feaff8943bb9cf99730acd298d
Jakob Pupke <jakob.pupke@googlemail.com>
2018-09-27 17:19:38 +0200

    fix LocalJumpError. Do not return in initializer.

--- lib/webvalve/railtie.rb
+++ lib/webvalve/railtie.rb
@@ -1,23 +1,25 @@
 module WebValve
   class Railtie < ::Rails::Railtie
     initializer 'webvalve.set_autoload_paths', before: :set_autoload_paths do |app|
-      return unless WebValve.enabled?
-      WebValve.config_paths << app.root
+      if WebValve.enabled?
+        WebValve.config_paths << app.root
 
-      WebValve.config_paths.each do |root|
-        app.config.eager_load_paths << root.join('webvalve').to_s
+        WebValve.config_paths.each do |root|
+          app.config.eager_load_paths << root.join('webvalve').to_s
+        end
       end
     end
 
     initializer 'webvalve.setup' do
-      return unless WebValve.enabled?
-      WebValve.config_paths.each do |root|
-        path = root.join('config', 'webvalve.rb').to_s
-        load path if File.exist?(path)
-      end
+      if WebValve.enabled?
+        WebValve.config_paths.each do |root|
+          path = root.join('config', 'webvalve.rb').to_s
+          load path if File.exist?(path)
+        end
 
-      config.after_initialize do
-        WebValve.setup
+        config.after_initialize do
+          WebValve.setup
+        end
       end
     end
   end
