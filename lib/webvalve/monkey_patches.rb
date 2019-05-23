1760b677a643490cd6e4a538c3380755344f4881
Sam Moore <sam@betterment.com>
2019-05-23 09:30:09 -0400

    add monkey patch to support webdrivers gem

---
+++ lib/webvalve/monkey_patches.rb
@@ -0,0 +1,23 @@
+# if the webdrivers gem is included, patch it so that it can make HTTP requests to download
+# drivers when necessary.
+#
+# based on the v3.9.4 release of webdrivers gem
+# https://github.com/titusfortner/webdrivers/blob/v3.9.4/lib/webdrivers/network.rb#L47-L54
+#
+# and the v3.5.1 release of webmock gem
+# https://github.com/bblimke/webmock/blob/v3.5.1/lib/webmock/http_lib_adapters/net_http.rb#L12
+if defined?(Webdrivers)
+  class Webdrivers::Network
+    class << self
+      def http
+        client = WebMock::HttpLibAdapters::NetHttpAdapter::OriginalNetHTTP
+        if using_proxy
+          client.Proxy(Webdrivers.proxy_addr, Webdrivers.proxy_port,
+                       Webdrivers.proxy_user, Webdrivers.proxy_pass)
+        else
+          client
+        end
+      end
+    end
+  end
+end
