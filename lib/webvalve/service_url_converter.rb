4f797592e2a61a02b115bdce6cd0ec871e25aef8
John Mileham <john@betterment.com>
2023-07-13 14:28:47 -0400

    Rename general purpose boundary chars away from wildcard

--- lib/webvalve/service_url_converter.rb
+++ lib/webvalve/service_url_converter.rb
@@ -1,8 +1,8 @@
 module WebValve
   class ServiceUrlConverter
-    WILDCARD_BOUNDARY_CHARS = ('\b' + Regexp.escape('.:/?#@&=')).freeze
-    WILDCARD_SUBSTITUTION = ('[^' + WILDCARD_BOUNDARY_CHARS + ']*').freeze
-    URL_PREFIX_BOUNDARY = ('[' + WILDCARD_BOUNDARY_CHARS + ']').freeze
+    TOKEN_BOUNDARY_CHARS = ('\b' + Regexp.escape('.:/?#@&=')).freeze
+    WILDCARD_SUBSTITUTION = ('[^' + TOKEN_BOUNDARY_CHARS + ']*').freeze
+    URL_PREFIX_BOUNDARY = ('[' + TOKEN_BOUNDARY_CHARS + ']').freeze
     URL_SUFFIX_PATTERN = ('(' + URL_PREFIX_BOUNDARY + '.*)?\z').freeze
 
     attr_reader :url
