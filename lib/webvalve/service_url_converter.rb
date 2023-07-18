ad2775cea5ec555fdef1a2262d486c5a1d0e20f5
John Mileham <john@betterment.com>
2023-07-17 22:30:10 -0400

    Factor out `#template` for legibility

--- lib/webvalve/service_url_converter.rb
+++ lib/webvalve/service_url_converter.rb
@@ -2,48 +2,76 @@ require 'addressable/uri'
 require 'addressable/template'
 
 module WebValve
+  # @api private
   class ServiceUrlConverter
-
-    attr_reader :url
 
     def initialize(url:)
       @url = url
     end
 
     def template
-      if url.is_a?(String)
-        working_url = url.gsub('*', '**')
-        protocol = ""
-        if matchdata = %r{\A([^:]+:)(//.*)\z}.match(working_url)
-          protocol = matchdata[1]
-          working_url = matchdata[2]
-        end
+      if @url.is_a?(String)
+        protocol, working_url = split_protocol(double_stars(@url))
         uri = Addressable::URI.parse(working_url)
 
         if uri.fragment.present?
-          raise "Webvalve: URL Fragments are not valid: #{url}"
+          raise "Webvalve: URL Fragments are not valid: #{@url}"
         end
 
-        suffix = ""
+        suffix = extend_path_and_query!(uri)
 
-        if uri.query.present?
-          uri.query = "#{uri.query}{&ext*}"
-        else
-          if uri.path == "/" || uri.path == ""
-            uri.path = ""
-            suffix += "{/path*}"
-          else
-            uri.path = "#{uri.path}{/ext*}"
-          end
-
-          suffix += "{?query}"
-        end
-
-        substituted_url = (protocol + uri.to_s).gsub('**', '{star}')
-        Addressable::Template.new(substituted_url + suffix)
+        substituted_url = substitute_double_stars(protocol + uri.to_s + suffix)
+        Addressable::Template.new(substituted_url)
       else
         url
       end
+    end
+
+    private
+
+    # Doubles asterisks to retain Addressable::URL parseability while making
+    # wildcards distinct from the asterisks we'll be adding later. That way
+    # we can later gsub double asterisks to Addressable::Template variables
+    # in `#substitute_double_stars`
+    def double_stars(working_url)
+      working_url.gsub('*', '**')
+    end
+
+    # Splits protocol off to keep Addressable::URL from rejecting a scheme with
+    # asterisks in it. We'll recombine later. Returns [protocol, url]
+    def split_protocol(working_url)
+      if matchdata = %r{\A([^:]+:)(//.*)\z}.match(working_url)
+        [matchdata[1], matchdata[2]]
+      else
+        ['', working_url]
+      end
+    end
+
+    # Munges the provided URI object to append extensible path and query
+    # variables. If neither query nor path are present, returned suffix
+    # will include path and/or query patterns.
+    def extend_path_and_query!(uri)
+      suffix = ''
+
+      if uri.query.present?
+        uri.query = "#{uri.query}{&ext*}"
+      else
+        if uri.path == '/' || uri.path == ''
+          uri.path = ''
+          suffix += '{/path*}'
+        else
+          uri.path = "#{uri.path}{/ext*}"
+        end
+
+        suffix += '{?query}'
+      end
+
+      suffix
+    end
+
+    # substitutes double asterisks for `{star}` patterns
+    def substitute_double_stars(working_url)
+      working_url.gsub('**', '{star}')
     end
   end
 end
